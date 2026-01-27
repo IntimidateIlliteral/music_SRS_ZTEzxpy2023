01838714d0eef821842d7dd159728ccd51147dae
WuYihao <3019234536@tju.edu.cn>
2026-01-27 15:55:49 +0800

    -nojvm means what?

--- eg_not_divid_pilot_github_code_ignore/foodAndDormitory.sh
+++ eg_not_divid_pilot_github_code_ignore/foodAndDormitory.sh
@@ -27,7 +27,7 @@ NUM_INSTANCES=3
 for ((input0=1; input0<=$NUM_INSTANCES; input0++)); do
 # 如果不开多窗口，则matlab在计算出结果disp()后，不会exit，原因未知。
     gnome-terminal -- bash -c "
-        matlab -nodesktop -nosplash \
+        matlab -nodesktop -nosplash -nojvm \
             -r \"linux_input_column=$input0; linux_input_toggle=logical($input1); runMe4linux; exit\"
     "
 done
